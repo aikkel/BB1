2addc2c7bf002451fe038c060dcffdd8c10122b0
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-12-04 13:16:13 +0100

    clear sc

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var path = require('path'); // Step 1: Import the path module
 var router = express.Router();
+
+// Step 2: Serve static files
+router.use(express.static(path.join(__dirname, 'public', 'html')));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // Step 3: Send the index.html file
+  res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/minside', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/minside', function(req, res, next){
+  if ((req.body.username == 'Jens') && (req.body.password == 'Gut')) {
+    res.send('logget ind ' + req.body.username + " " + req.body.password);
+  }
+  else {
+    res.send('forkert username eller password');
+  }
 });
 
 module.exports = router;
