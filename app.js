d6eb690af437751e8eca5ea14207737f8f94fbde
aikkel <mikk39k8@edu.zealand.dk>
2023-12-04 10:24:33 +0100

    converted html to .jade and port 3000 to 8080

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.get('/', function(req, res) {
 
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
 });
