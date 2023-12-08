7153b409908c72c11527d4cfda52cdddcb6d3496
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-12-08 13:53:04 +0100

    commimttng

---
+++ public/javascripts/index.js
@@ -0,0 +1,6 @@
+function redirectToAdvert(advertID) {
+    // Redirect to the advertisement page using the advertID
+    window.location.href = `/adverts/${advertID}`;
+}
+
+
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bloby' });
 });
 
+// Define route for displaying advertisement details
+router.get('/adverts/:advertID', (req, res) => {
+  const advertID = req.params.advertID;
+
+  // Replace this with your actual database query to fetch advertisement details
+  // Example using Mongoose:
+  // Advertisement.findById(advertID, (err, advert) => {
+  //   if (err || !advert) {
+  //     // Handle error or not found scenario
+  //   } else {
+  //     // Render a page with the advertisement details
+  //     res.render('advert-details', { advert });
+  //   }
+  // });
+
+  // For demonstration purposes, sending a sample response
+  res.send(`Displaying advertisement details for ID: ${advertID}`);
+});
+
 
 module.exports = router;
