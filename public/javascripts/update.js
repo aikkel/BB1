d4bf868b2eb5674e3380e0048b418e223b8fff95
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-12-12 17:22:51 +0100

    Update database, JavaScript, and views files

---
+++ public/javascripts/update.js
@@ -0,0 +1,13 @@
+function logToConsole() {
+    alert("Du er nu logget ud");
+    document.cookie = 'cookieName=; expires=Thu, 01 Jan 3000 00:00:00 UTC; path=/;';
+  }
+  document.getElementById('deleteCookiesButton').addEventListener('click', function() {
+    deleteCookies();
+  });
+
+  document.cookie = "cookiename=cookievalue"
+
+function sikkerhed() {
+  alert("Er du sikker på at du ville ændre dine oplysninger?")
+}
