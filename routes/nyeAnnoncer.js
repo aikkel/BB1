84b279823ead610ec3f5cc37b380c8966b0b6d05
StefanDHallberg <Stefanditlev@gmail.com>
2023-12-12 10:26:29 +0100

    WIP

--- routes/nyeAnnoncer.js
+++ routes/nyeAnnoncer.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-// GET route for the "nyeAnnoncer" view
+
+
+
+// Antag at du har en funktion, der henter brugerinformation
+// Denne funktion er blot et eksempel og skal tilpasses din applikation
+function getBrugerInfo() {
+  // Her skal du hente den faktiske brugerinformation
+  return {
+    email: 'bruger@example.com',
+    phone: '12345678'
+  };
+}
+
+// GET-rute for "nyeAnnoncer" view
 router.get('/', (req, res) => {
-    // Logic to fetch and render the "nyeAnnoncer" view
-    res.render('nyeAnnoncer');
+  const brugerInfo = getBrugerInfo(); // Hent brugerinformation
+  res.render('nyeAnnoncer', { brugerInfo }); // Send brugerInfo til skabelonen
 });
+
+
 
 module.exports = router;
