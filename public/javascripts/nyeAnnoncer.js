9f4b3376eb6347d0437fd4057a56dcc69a8c343e
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-12-13 11:05:00 +0100

    Hurtigt tilføjelse af emner fra kunden

---
+++ public/javascripts/nyeAnnoncer.js
@@ -0,0 +1,9 @@
+function logToConsole() {
+    alert("Du er nu logget ud");
+    document.cookie = 'cookieName=; expires=Thu, 01 Jan 3000 00:00:00 UTC; path=/;';
+  }
+  document.getElementById('deleteCookiesButton').addEventListener('click', function() {
+    deleteCookies();
+  });
+  
+  document.cookie = "cookiename=cookievalue"
